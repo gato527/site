80fde734e49502f26a5902f6378db2fd1e58c19f
gato527 <juanchodave1.999@gmail.com>
2022-08-04 19:45:51 -0500

    empezando

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <title>WEBSITE INBOX</title>   
+</head>
+<body>
+    <!-- <header>
+        <nav></nav>
+      <h1>Encabezado</h1>
+    </header> -->
+    <main>
+        <div class="container">
+            <div class="box">
+
+            <h1>hola</h1>
+
+            </div>
+        </div>
+    </main>
+<!-- 
+    <footer>
+        <h1>footer</h1>
+    </footer> -->
+    
+
+    
+</body>
+</html>
