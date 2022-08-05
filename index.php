b3b3a1392a62e4cab47a8ddb7a166ca7cc032a5b
gato527 <juanchodave1.999@gmail.com>
2022-08-05 08:27:41 -0500

    colocando div centro

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>WEBSITE INBOX</title>   
 </head>
 <body>
@@ -13,13 +14,16 @@
       <h1>Encabezado</h1>
     </header> -->
     <main>
-        <div class="container">
-            <div class="box">
-
-            <h1>hola</h1>
-
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <h1>hola</h1>
+                    </div>
+                    <div class="col-6">
+                        <h1>juan</h1>                       
+                    </div>
+                </div>
             </div>
-        </div>
     </main>
 <!-- 
     <footer>
